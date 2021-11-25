5ed9981513118f4dbc7f54fd711f7c92ee69e9d0
Jacob <jckob21@gmail.com>
2021-11-25 16:37:13 +0000

    Added menu bar and internal css

--- index.php
+++ index.php
@@ -5,10 +5,20 @@
         <meta charset="utf-8">
         <title>Student Record Card</title>
         <meta name="description" content="Swansea University Student Record Card">
+    
+        <style>
+            h1 {
+                margin: 0px;
+                padding: 2px;
+
+            }
+        </style>
     </head>
 
     <body>
-        <?php
+        <img src="SUmenubar.png" alt="S W A N S E A   U N I V E R S I T Y   M E N U   B A R">
+        <h1>Student Record Card</h1>
+    <?php
             echo "<p>Init</p>";
         ?>
 
