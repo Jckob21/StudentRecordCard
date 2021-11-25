7a83e69e671c3dbbcaa23b8dc8d9487f2b510131
Jacob <jckob21@gmail.com>
2021-11-25 16:28:46 +0000

    Init php file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Student Record Card</title>
+        <meta name="description" content="Swansea University Student Record Card">
+    </head>
+
+    <body>
+        <?php
+            echo "<p>Init</p>";
+        ?>
+
+    </body>
+</html>
