e91ed7a699fe4ffa7bcec6264ac7e75ebdf1208e
Jacob <jckob21@gmail.com>
2021-11-26 14:58:34 +0000

    Implemented query to retain module selection information

--- index.php
+++ index.php
@@ -228,6 +228,50 @@
 
         <hr>
         <h3>Module Selection</h3>
+<?php
+//////////////////////////  Enrolment and Progress  ///////////////////////////
+    if(isset($_POST["studID"]))
+    {
+        
+        $query =    "SELECT smod.ayr, smod.mid, mtitle, credits FROM enrl, smod, mods WHERE enrl.ayr = smod.ayr AND enrl.sid = smod.sid AND smod.mid = mods.mid AND enrl.sid = " . $_POST["studID"] . " ORDER BY smod.ayr desc, smod.mid desc";
+        $result = mysqli_query($connection, $query);
+        
+        // print year and first module
+        $row = mysqli_fetch_array($result);
+        echo "<table>";
+        echo    "<tr>";
+        echo        '<th colspan="3">' . $row["ayr"] . '</th>';
+        echo    "</tr>";
+        //first module
+        echo "<tr>";
+        echo    "<td>" . $row["mid"] . "</td>";
+        echo    "<td>" . $row["mtitle"] . "</td>";
+        echo    "<td>" . $row["credits"] . "</td>";
+        echo "</tr>";
+
+        $currentYear = $row["ayr"];
+
+        while($row = mysqli_fetch_array($result))
+        {
+            if($currentYear != $row["ayr"])
+            {
+                echo    "<tr>";
+                echo        '<th colspan="3">' . $row["ayr"] . '</th>';
+                echo    "</tr>";
+                $currentYear = $row["ayr"];
+            }
+
+            echo "<tr>";
+            echo    "<td>" . $row["mid"] . "</td>";
+            echo    "<td>" . $row["mtitle"] . "</td>";
+            echo    "<td>" . $row["credits"] . "</td>";
+            echo "</tr>";
+        }
+        
+        echo "</table>";
+    }
+?>
+
         <table>
             <tr>
                 <th colspan="3">2001/2002</th>
