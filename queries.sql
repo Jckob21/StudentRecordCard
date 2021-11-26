2ed30ba12c64bb7a753f438c0f456856612098c1
Jacob <jckob21@gmail.com>
2021-11-26 01:24:09 +0000

    Edited query so now it is ordered by level in descending order

--- queries.sql
+++ queries.sql
@@ -5,6 +5,6 @@ SELECT * FROM stud WHERE sid = "166296";
 SELECT enrl.pid, paward, ptitle, prog.did, length, dname FROM enrl, prog, dept WHERE enrl.pid=prog.pid AND prog.did=dept.did AND sid=166296 GROUP BY pid
 
 -- GET ENROLMENT AND PROGRESS DETAILS AND COURSE TITLE
-SELECT * FROM enrl, stud, prog WHERE enrl.sid = stud.sid AND enrl.pid = prog.pid AND enrl.sid = 166296;
+SELECT * FROM enrl, stud, prog WHERE enrl.sid = stud.sid AND enrl.pid = prog.pid AND enrl.sid = 166296 ORDER BY lvl DESC;
 
 -- GET MODULE SELECTION DETAILS
