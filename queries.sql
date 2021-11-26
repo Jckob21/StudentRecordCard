e83934f459a1554f6654062d71e8e363810d0ea8
Jacob <jckob21@gmail.com>
2021-11-26 01:08:02 +0000

    updated query to get course details, now it includes department name

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 SELECT * FROM stud WHERE sid = "166296";
 
 -- GET COURSE DETAILS
-SELECT enrl.pid, paward, ptitle, did, length FROM enrl, prog WHERE enrl.pid=prog.pid AND sid=166296 GROUP BY pid;
+SELECT enrl.pid, paward, ptitle, prog.did, length, dname FROM enrl, prog, dept WHERE enrl.pid=prog.pid AND prog.did=dept.did AND sid=166296 GROUP BY pid
 
 -- GET ENROLMENT AND PROGRESS DETAILS AND COURSE TITLE
 SELECT * FROM enrl, stud, prog WHERE enrl.sid = stud.sid AND enrl.pid = prog.pid AND enrl.sid = 166296;
