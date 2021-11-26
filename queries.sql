49e637c5ed9279335e7dd12c988d6d8970243d7e
Jacob <jckob21@gmail.com>
2021-11-26 18:43:56 +0000

    Updated query for COURSE DETAILS, now it is ordered, starting from most recent one

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 SELECT * FROM stud WHERE sid = "166296";
 
 -- GET COURSE DETAILS
-SELECT enrl.pid, paward, ptitle, prog.did, length, dname FROM enrl, prog, dept WHERE enrl.pid=prog.pid AND prog.did=dept.did AND sid=166296 GROUP BY pid
+SELECT enrl.pid, paward, ptitle, prog.did, length, dname FROM enrl, prog, dept WHERE enrl.pid=prog.pid AND prog.did=dept.did AND sid=166296 GROUP BY pid ORDER BY lvl DESC;
 
 -- GET ENROLMENT AND PROGRESS DETAILS AND COURSE TITLE
 SELECT * FROM enrl, stud, prog WHERE enrl.sid = stud.sid AND enrl.pid = prog.pid AND enrl.sid = 166296 ORDER BY lvl DESC;
